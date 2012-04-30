dcf82d2418dcfbdf5dab29f32664ad542efbc31c
Yutaka Kamioka <umaibow@gmail.com>
2012-04-30 13:24:59 +0900

    * バージョン0.4 * ファイル名変更string.xml→labels.xml

--- src/jp/kamioka/timecard/Const.java
+++ src/jp/kamioka/timecard/Const.java
@@ -2,5 +2,5 @@ package jp.kamioka.timecard;
 
 public class Const {
     public static final String NAME = "TimeCardPuncher";
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.4";
 }
